07148d9d97372a7a98b6cc50fe0f00384ac934ac
tsukhu <tsukhu71@gmail.com>
2014-05-14 10:17:12 +0530

    test

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+// tests.js
+
+var chai = require('chai');
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present.', function(){
+      chai.assert.equal(-1, [1,2,3].indexOf(5));
+      chai.assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
